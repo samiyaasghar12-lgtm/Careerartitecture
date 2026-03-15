4b5dc5bd06ec6a4eebf7f2026b46996b0d6f421e
Samiya Asghar <samiyaasghar12@gmail.com>
2026-03-15 03:59:58 -0700

    Add comments and  fix code

--- agent.py
+++ agent.py
@@ -1,18 +1,20 @@
+# import
 import google.generativeai as genai
 
-# Aapki API Key (Jo screenshot mein thi)
+#select API Key
 genai.configure(api_key="AIzaSyDUcsqMXXxc053uX8E1huVE9wN5SUk3YX8")
 
-# Madam wala model version
+
+# select modal
 model=genai.GenerativeModel("gemini-2.5-flash-lite")
 
 print("Chat started (Type 'exit' or 'bye' or 'quit')")
-
+# apply while loop
 while True:
     user_input=input("you!")
     if user_input in ['exit','bye','quit']:
         print("Goodbye! see you tomorrow!")
         break
-    
+    # user input passing
     response=model.generate_content(user_input)
     print("Agent:", response.text)
